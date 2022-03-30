d94409739ef1caa102016da044c3583818812ff5
giacomo <88339527+gi-ba-bu@users.noreply.github.com>
2022-03-30 16:28:28 +0200

    generate id

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
-app = Flask(__name__)
+app = Flask(__name__)  # noqa: E402
+
+urls = {"abc", "cde"}  # noqa: E402
+
+from random import choice  # noqa: E402
+import string  # noqa: E402
+
+
+def generate_short_id(num_of_chars: int):
+    # """Function to generate short_id of specified number of characters"""
+    return ''.join(choice(string.ascii_letters+string.digits) for _ in range(num_of_chars))
 
 
 @app.route('/')
@@ -8,17 +18,27 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/create_corto_url', methods=['GET', 'POST'])
-def create_corto_url():
+@app.route('/corto', methods=['GET', 'POST'])
+def create_corto():
     if request.method == 'POST':
-        pass
+        corto_id = generate_short_id(8)
+        print(f"hello I have been posted {corto_id}")
         # print(request.form)
         # piece_count = request.form['piece_count']
         # result = pricer.predict(piece_count)
-        # return render_template('predict.html', default=piece_count, title='Result', result=result)
+        return render_template('home.html', title='Result')
     else:
         pass
         # return render_template('predict.html', default=0, result=0, title='Predict')
+
+
+@app.route('/corto/<string_id>', methods=['GET', 'POST'])
+def corto_url(string_id):
+    if request.method == 'GET':
+        if string_id in urls:
+            return redirect("http://www.example.com", code=302)
+        else:
+            return redirect("/", code=302)
 
 
 if __name__ == "__main__":
